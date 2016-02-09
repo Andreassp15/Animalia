bd2e2c1ec62b4285ecc214f35d5bb9e51beca679
Ronja Bergman <ronjaskola@hotmail.com>
2016-02-09 11:52:33 +0100

    testbanan

---
+++ Animalia_Prototype_0.1/Assets/Scripts/Move_Whagon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_Whagon : MonoBehaviour
+{
+    public Rigidbody Whagon;
+
+    void Start ()
+    {
+        Whagon = GetComponent<Rigidbody>();
+        Whagon.AddForce(0f, 0f, 15f, ForceMode.Impulse);
+    }
+	
+	void FixedUpdate ()
+    {
+    
+    }
+}

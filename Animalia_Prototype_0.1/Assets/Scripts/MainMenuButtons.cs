aeffa87541a4afe6ceab40f2a7bcf6425d1ae154
Mbz1011 <jmikib@gmail.com>
2016-02-11 17:07:54 +0100

    meny

--- Animalia_Prototype_0.1/Assets/Scripts/MainMenuButtons.cs
+++ Animalia_Prototype_0.1/Assets/Scripts/MainMenuButtons.cs
@@ -3,10 +3,35 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MainMenuButtons : MonoBehaviour {
+    public GameObject MenuButton1;
+    public GameObject MenuButton2;
+    public GameObject MenuButton3;
+    public GameObject HelpButton1;
+    public GameObject HelpButton2;
+    public GameObject HelpButton3;
+    public GameObject[] helppics;
+    int currentPic;
 
-	
-	
-	public void Onclick(int button)
+    public void RightButton()
+    {
+        helppics[currentPic].SetActive(false);
+        currentPic = (currentPic + 1) % helppics.Length;
+        helppics[currentPic].SetActive(true);
+
+    }
+    public void LeftButton()
+    {
+        helppics[currentPic].SetActive(false);
+        currentPic--;
+        if (currentPic < 0)
+        {
+            currentPic = helppics.Length-1;
+        }
+        helppics[currentPic].SetActive(true);
+    
+    }
+
+    public void Onclick(int button)
     {
         if(button == 1)//Loads the game
         {
@@ -17,6 +42,36 @@ public class MainMenuButtons : MonoBehaviour {
         {
             Application.Quit();
         }
+        if (button == 3)
+        {
+            helppics[currentPic].SetActive(true);
+            HelpButton1.SetActive(true);
+            HelpButton2.SetActive(true);
+            HelpButton3.SetActive(true);
+            MenuButton1.SetActive(false);
+            MenuButton2.SetActive(false);
+            MenuButton3.SetActive(false);
+        }
+
+        if(button == 4)
+        {
+            RightButton();
+        }
+        if(button == 5)
+        {
+            LeftButton();
+        }
+        if(button == 6)
+        {
+            helppics[currentPic].SetActive(false);
+            HelpButton1.SetActive(false);
+            HelpButton2.SetActive(false);
+            HelpButton3.SetActive(false);
+            MenuButton1.SetActive(true);
+            MenuButton2.SetActive(true);
+            MenuButton3.SetActive(true);
+        }
+       
 
 
     }
